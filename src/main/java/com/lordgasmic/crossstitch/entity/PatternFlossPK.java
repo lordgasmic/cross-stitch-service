8fa71405f0130f0bcf86e8c211d61652ef213500
Nick Wellman <wellman.nicholas@gmail.com>
2020-06-09 23:14:40 -0400

    refactor db

--- src/main/java/com/lordgasmic/crossstitch/entity/PatternFlossPK.java
+++ src/main/java/com/lordgasmic/crossstitch/entity/PatternFlossPK.java
@@ -3,10 +3,11 @@ package com.lordgasmic.crossstitch.entity;
 import lombok.Data;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Embeddable
 @Data
-public class PatternFlossPK {
+public class PatternFlossPK implements Serializable {
 
     private int pid;
     private int fid;
